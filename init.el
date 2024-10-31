b992be2a877f13cdfde996467700781e5683312e
Zhengyi Fu <i@fuzy.me>
2024-10-31 16:19:40 +0800

    init.el: grep--heading-filter is not available before emacs 30

--- init.el
+++ init.el
@@ -580,12 +580,14 @@ value for USE-OVERLAYS."
 (use-package embark-consult
   :defer t
   :config
-  (defun +embark-consult-export-grep--headings (&rest _)
-    (save-excursion
-      (goto-char (point-max))
-      (let ((inhibit-read-only t))
-        (grep--heading-filter))))
-  (advice-add #'embark-consult-export-grep :after #'+embark-consult-export-grep--headings))
+  (static-if (fboundp 'grep--heading-filter)
+      (progn
+        (defun +embark-consult-export-grep--headings (&rest _)
+          (save-excursion
+            (goto-char (point-max))
+            (let ((inhibit-read-only t))
+              (grep--heading-filter))))
+        (advice-add #'embark-consult-export-grep :after #'+embark-consult-export-grep--headings))))
 
 
 (use-package consult-dir
