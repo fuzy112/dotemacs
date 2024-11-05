9b637d007bd0d22c6a6cb0fa9479b7f97a66cdd9
Zhengyi Fu <i@fuzy.me>
2024-11-05 10:17:59 +0800

    init.el Fix indentation

--- init.el
+++ init.el
@@ -1389,26 +1389,25 @@ minibuffer."
   (setq rime-show-candidate 'posframe)
 
   (define-advice rime--posframe-display-content (:override (content))
-  "Display CONTENT with posframe."
-  (if (and (featurep 'posframe) (posframe-workable-p))
-      (if (string-blank-p content)
-          (posframe-hide rime-posframe-buffer)
-        (let*
-            ((preedit (rime--current-preedit))
-             (x (cond
-                 ((not rime-posframe-fixed-position) 0)
-                 ((not preedit) 0)
-                 ((not (overlayp rime--preedit-overlay)) 0)
-                 (t (rime--string-pixel-width preedit)))))
-          (apply #'posframe-show rime-posframe-buffer
-                 :string content
-                 :x-pixel-offset (- x)
-                 :background-color (face-attribute 'rime-default-face :background nil t)
-                 :foreground-color (face-attribute 'rime-default-face :foreground nil t)
-                 rime-posframe-properties)))
-    ;; Fallback to popup when not available.
-    (rime--popup-display-content content)))
-)
+    "Display CONTENT with posframe."
+    (if (and (featurep 'posframe) (posframe-workable-p))
+        (if (string-blank-p content)
+            (posframe-hide rime-posframe-buffer)
+          (let*
+              ((preedit (rime--current-preedit))
+               (x (cond
+                   ((not rime-posframe-fixed-position) 0)
+                   ((not preedit) 0)
+                   ((not (overlayp rime--preedit-overlay)) 0)
+                   (t (rime--string-pixel-width preedit)))))
+            (apply #'posframe-show rime-posframe-buffer
+                   :string content
+                   :x-pixel-offset (- x)
+                   :background-color (face-attribute 'rime-default-face :background nil t)
+                   :foreground-color (face-attribute 'rime-default-face :foreground nil t)
+                   rime-posframe-properties)))
+      ;; Fallback to popup when not available.
+      (rime--popup-display-content content))))
 
 ;;;; kinsoku
 
