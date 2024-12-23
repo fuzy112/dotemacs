bb513a654aeb824716190a807e7e6fcd71bd7fcf
Zhengyi Fu <i@fuzy.me>
2024-12-23 14:57:20 +0800

    Refactor dynamic-highlight

--- site-lisp/dynamic-highlight.el
+++ site-lisp/dynamic-highlight.el
@@ -1,4 +1,24 @@
-;; -*- lexical-binding: t; -*-
+;;; dynamic-highlight.el --- Highlight occurrences of symbol at point -*- lexical-binding: t -*-
+;; Copyright © 2024  Zhengyi Fu <i@fuzy.me>
+
+;; Author:   Zhengyi Fu <i@fuzy.me>
+;; Package-Requires: ((emacs "31.0.50"))
+;; Version: 0.1.0
+;; Keywords: font-lock
+
+;;; Commentary:
+
+;; This is a local minor mode.  When activated, occurrences of the
+;; symbol at point in the current buffer will be highlighted with
+;; the `dynamic-highlight' face.
+;;
+;; To enable Dynamic Highlight mode in CC modes, add the following
+;; code to your init.el:
+;;
+;; (add-hook \\='c-mode-common-hook #\\='dynamic-highlight-mode)
+;;
+
+;;; Code:
 
 (require 'hi-lock)
 
@@ -19,39 +39,33 @@
   "Number of seconds to delay before highlighting the symbol at point."
   :type 'number)
 
-(defcustom dynamic-highlight-predicate
-  (lambda () (derived-mode-p 'prog-mode))
-  "Wether to highlight the symbol."
+(defcustom dynamic-highlight-predicate #'always
+  "Whether to highlight the symbol."
   :type 'function)
 
 (defvar dynamic-highlight--timer nil)
-
-(defvar dynamic-highlight--regexp nil)
-(defvar dynamic-highlight--buffer nil)
+(defvar-local dynamic-highlight--regexp nil)
 
 (defun dynamic-highlight--update ()
   (and dynamic-highlight--regexp
-       dynamic-highlight--buffer
-    (with-current-buffer dynamic-highlight--buffer
-      (hi-lock-unface-buffer dynamic-highlight--regexp)
-      (setq dynamic-highlight--regexp nil)))
-  (when-let* ((symbol (thing-at-point 'symbol))
-	      ((funcall dynamic-highlight-predicate)))
-    (setq dynamic-highlight--regexp (regexp-opt (list symbol) 'symbols)
-	  dynamic-highlight--buffer (current-buffer))
+       (hi-lock-unface-buffer dynamic-highlight--regexp)
+       (setq dynamic-highlight--regexp nil))
+  (when-let* (((symbol-value 'dynamic-highlight-mode))
+	      (symbol (thing-at-point 'symbol))
+	      ((funcall dynamic-highlight-predicate symbol)))
+    (setq dynamic-highlight--regexp (regexp-opt (list symbol) 'symbols))
     (let ((hi-lock-use-overlays t))
       (hi-lock-set-pattern dynamic-highlight--regexp 'dynamic-highlight))))
 
 ;;;###autoload
 (define-minor-mode dynamic-highlight-mode
   "Highlight all occurrences of the symbol at point."
-  :global t
-  (when (timerp dynamic-highlight--timer)
-    (cancel-timer dynamic-highlight--timer))
+  :global nil
   (when dynamic-highlight-mode
-    (setq dynamic-highlight--timer
-	  (run-with-idle-timer
-	   dynamic-highlight-delay t #'dynamic-highlight--update))))
+    (or dynamic-highlight--timer
+	(setq dynamic-highlight--timer
+	      (run-with-idle-timer
+	       dynamic-highlight-delay t #'dynamic-highlight--update)))))
 
 (provide 'dynamic-highlight)
 ;;; dynamic-highlight.el ends here
