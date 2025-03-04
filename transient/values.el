ce1022bc2d9d694b0381150e1818aec444d381a4
Zhengyi Fu <i@fuzy.me>
2025-03-04 16:57:36 +0800

    Add --gpg-sign to more prefix
    * transient/values.el:
    * transient/levels.el: Add --gpg-sign to more prefix.

--- transient/values.el
+++ transient/values.el
@@ -4,4 +4,6 @@
   ("--languages=" "C" "C++" "CMake")
   "--extras=*" "--fields=*" "--kinds-all=*" "--roles-all=*")
  (gptel-menu "g*quick-answer*")
- (magit-commit "--gpg-sign=ABE50B31E2F0C94AC4585BC78D97BF3F6BFA0BDA"))
+ (magit-commit "--gpg-sign=ABE50B31E2F0C94AC4585BC78D97BF3F6BFA0BDA")
+ (magit-merge "--gpg-sign=ABE50B31E2F0C94AC4585BC78D97BF3F6BFA0BDA")
+ (magit-rebase "--autostash" "--gpg-sign=ABE50B31E2F0C94AC4585BC78D97BF3F6BFA0BDA"))
