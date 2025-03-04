b41ce744adad603923278e333badc09b28b93a60
Zhengyi Fu <i@fuzy.me>
2025-03-04 16:57:36 +0800

    Add --gpg-sign to more prefix
    * transient/values.el:
    * transient/levels.el: Add --gpg-sign to more prefix.

--- transient/levels.el
+++ transient/levels.el
@@ -1,4 +1,10 @@
 ((magit-commit
   (magit:--signoff . 4)
   (magit-commit:--date . 4)
+  (magit:--gpg-sign . 4))
+ (magit-merge
+  (magit:--signoff . 4)
+  (magit:--gpg-sign . 4))
+ (magit-rebase
+  (magit:--signoff . 4)
   (magit:--gpg-sign . 4)))
