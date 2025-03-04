667e47e741218ac148078c37326cb665269f0588
Zhengyi Fu <i@fuzy.me>
2025-03-04 14:58:01 +0800

    Update transient config
    * transient/levels.el: New file.  Modify default levels for some magit
    commit commands.
    * transient/values.el (gptel-menu): Add --gpg-sign= option.

--- transient/values.el
+++ transient/values.el
@@ -3,4 +3,5 @@
   "--recurse"
   ("--languages=" "C" "C++" "CMake")
   "--extras=*" "--fields=*" "--roles-all=*")
- (gptel-menu "g*quick-answer*"))
+ (gptel-menu "g*quick-answer*")
+ (magit-commit "--gpg-sign=ABE50B31E2F0C94AC4585BC78D97BF3F6BFA0BDA"))
