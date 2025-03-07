2696430e13f1a3462628fb8df36c556c77a69d86
Zhengyi Fu <i@fuzy.me>
2025-03-07 10:26:53 +0800

    Refactor activities-persist-buffers
    * site-lisp/activities-persist-buffers.el (activities-persist-buffers-ant-save-predicates):
    New variable.
    * site-lisp/activities-persist-buffers.el (activities-persist-buffers--error-buffer-p):
    (activities-persist-buffers--backtrace-p):
    (activities-persist-buffers-ant-save-predicates): New function.
    (activities-persist-buffers-activity-state): Renamed from
    activities-activity-state-with-buffers.
    (activities-persist-buffers--save-activity-buffers): Renamed from
    +activities-state--with-buffers.
    (activities-persist-buffers--restore-buffers):
    Renamed from +activities--restore-buffers.
    (activities-persist-buffers--restore-activity-buffers): Renamed from
    +activities--restore-activity-buffers.

--- site-lisp/activities-persist-buffers.el
+++ site-lisp/activities-persist-buffers.el
@@ -29,13 +29,26 @@
 (require 'activities)
 (require 'activities-tabs)
 
-(cl-defstruct (activities-activity-state-with-buffers
+(cl-defstruct (activities-persist-buffers-activity-state
                (:include activities-activity-state))
   "activities-activity-state with buffers."
   buffer-list)
 
-(defun +activities-state--with-buffers (activity-state)
-  (make-activities-activity-state-with-buffers
+(defun activities-persist-buffers--error-buffer-p (buf)
+  (buffer-match-p "\\`\\*Activities (error):" buf))
+
+(defun activities-persist-buffers--backtrace-p (buf)
+  (buffer-match-p '(derived-mode . backtrace-mode) buf))
+
+(defvar activities-persist-buffers-ant-save-predicates
+  '(activities-buffer-special-p
+    activities-buffer-hidden-p
+    activities-persist-buffers--error-buffer-p
+    activities-persist-buffers--backtrace-p
+    minibufferp))
+
+(defun activities-persist-buffers--save-activity-buffers (activity-state)
+  (make-activities-persist-buffers-activity-state
    :window-state (activities-activity-state-window-state activity-state)
    :etc (activities-activity-state-etc activity-state)
    :buffer-list
@@ -43,36 +56,38 @@
                        'activities-buffer-list
                        (activities-tabs--tab (activities-current)))))
      (setq buffer-list (seq-filter #'buffer-live-p buffer-list))
-     (mapcar (lambda (buf)
-               (with-current-buffer buf
-                 (activity--serialize buf)))
-             buffer-list))))
+     (setq buffer-list
+           (seq-filter
+            (lambda (buf)
+              (not (run-hook-with-args-until-success
+                    'activities-persist-buffers-ant-save-predicates buf)))
+            buffer-list))
+     (mapcar #'activity--serialize buffer-list))))
 
-(defun +activities--restore-buffers (buffers)
+(defun activities-persist-buffers--restore-buffers (buffers)
   (let ((buffer-list (mapcar #'activities--deserialize buffers)))
-    (let* ((activity (activities-current))
-           (tab (activities-tabs--tab activity)))
+    (let* ((tab (tab-bar--current-tab-find)))
       (setf (alist-get 'activities-buffer-list (cdr tab)) buffer-list))))
 
-(cl-defun +activities--restore-activity-buffers (activity &key (state 'last))
+(cl-defun activities-persist-buffers--restore-activity-buffers (activity &key (state 'last))
   (activities-with activity
     (pcase-let (((cl-struct activities-activity name default last) activity))
       (pcase state
-        ('default (+activities--restore-buffers (activities-activity-state-with-buffers-buffer-list default)))
+        ('default (activities-persist-buffers--restore-buffers (activities-persist-buffers-activity-state-buffer-list default)))
         ('last (if last
-                   (+activities--restore-buffers (activities-activity-state-with-buffers-buffer-list last))
-                 (+activities--restore-buffers (activities-activity-state-with-buffers-buffer-list default))))))))
+                   (activities-persist-buffers--restore-buffers (activities-persist-buffers-activity-state-buffer-list last))
+                 (activities-persist-buffers--restore-buffers (activities-persist-buffers-activity-state-buffer-list default))))))))
 
 ;;;###autoload
 (define-minor-mode activities-persist-buffers-mode
   "Activities-Persist-Buffers mode."
   :global t
-  (advice-remove 'activities-state #'+activities-state--with-buffers)
-  (advice-remove 'activities-set #'+activities--restore-activity-buffers)
+  (advice-remove 'activities-state #'activities-persist-buffers--save-activity-buffers)
+  (advice-remove 'activities-set #'activities-persist-buffers--restore-activity-buffers)
   (when activities-persist-buffers-mode
     (activities-tabs-mode)
-    (advice-add 'activities-state :filter-return #'+activities-state--with-buffers)
-    (advice-add 'activities-set :after #'+activities--restore-activity-buffers)))
+    (advice-add 'activities-state :filter-return #'activities-persist-buffers--save-activity-buffers)
+    (advice-add 'activities-set :after #'activities-persist-buffers--restore-activity-buffers)))
 
 (provide 'activities-persist-buffers)
 ;;; activity-persist-buffers.el ends here
