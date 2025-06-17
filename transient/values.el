7ed1719a283991d2a3526e380cce24c87ae3098f
Zhengyi Fu <i@fuzy.me>
2025-06-17 10:58:05 +0800

    * transient/values.el (gptel-menu): Add --prune --tags to `magit-fetch'.

--- transient/values.el
+++ transient/values.el
@@ -4,4 +4,5 @@
   ("--languages=" "C" "C++" "CMake")
   "--extras=*" "--fields=*" "--kinds-all=*" "--roles-all=*")
  (gptel-menu "g*quick-answer*")
+ (magit-fetch "--prune" "--tags")
  (magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature"))
