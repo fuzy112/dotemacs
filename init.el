33a43f8418b6f242d7958971abcec3cfc5a38622
Zhengyi Fu <i@fuzy.me>
2025-06-19 15:14:21 +0800

    * init.el (elfeed-show-tag): Fix calculation of the tags completion tags.

--- init.el
+++ init.el
@@ -2031,7 +2031,10 @@ fi"))
    (mapcar #'intern
            (completing-read-multiple
             "Tags: "
-            (seq-difference (mapcar #'intern +elfeed-tag-history)
+            (seq-difference (mapcar #'intern
+                                    (mapcan (lambda (s)
+                                              (split-string s crm-separator t "[[:space:]]"))
+                                            +elfeed-tag-history))
                             (elfeed-entry-tags elfeed-show-entry))
             nil nil nil '+elfeed-tag-history)))
   args)
