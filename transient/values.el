25d70982de8f634de720a14e59918fed70cc5e09
Zhengyi Fu <i@fuzy.me>
2025-06-23 09:37:26 +0800

    Enable --autostash in `magit-pull' by default
    * transient/levels.el (magit-pull): Set level of --autostash to 1.
    * transient/values.el (magit-pull): Add --autostash by default.

--- transient/values.el
+++ transient/values.el
@@ -5,4 +5,5 @@
   "--extras=*" "--fields=*" "--kinds-all=*" "--roles-all=*")
  (gptel-menu "g*quick-answer*")
  (magit-fetch "--prune" "--tags")
- (magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature"))
+ (magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature")
+ (magit-pull "--autostash"))
