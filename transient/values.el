739833af86054aeec094e3099e99d9fe87bf4acd
Zhengyi Fu <i@fuzy.me>
2025-07-17 10:05:32 +0800

    * transient/values.el (gptel-menu): Remove default value.

--- transient/values.el
+++ transient/values.el
@@ -3,7 +3,7 @@
   "--recurse"
   ("--languages=" "C" "C++" "CMake")
   "--extras=*" "--fields=*" "--kinds-all=*" "--roles-all=*")
- (gptel-menu "g*quick-answer*")
+ (gptel-menu)
  (magit-fetch "--prune" "--tags")
  (magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature")
  (magit-pull "--autostash"))
