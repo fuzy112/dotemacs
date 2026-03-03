f1cac503c49275807bc511ea3ef63fe126c95cb2
Zhengyi Fu <i@fuzy.me>
2026-03-03 15:38:39 +0800

    Configure Corfu and nerd-icons-corfu for Telega completions
    - Transform telega-company backends into Capf functions for Corfu
    - Configure nerd-icons-corfu mappings for Telega completion kinds
    - Add telega-capf-setup to telega-chat-mode-hook

--- user-lisp/telega-config.el
+++ user-lisp/telega-config.el
@@ -1,6 +1,6 @@
 ;; telega-config.el -- Telega configuration -*- lexical-binding: t; -*-
 
-;; Copyright © 2024-2025  Zhengyi Fu <i@fuzy.me>
+;; Copyright © 2024-2026  Zhengyi Fu <i@fuzy.me>
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -50,6 +50,43 @@
     (apply args)))
 
 (add-hook 'telega-root-mode-hook #'hl-line-mode)
+
+;; Telega company
+
+;; Transform telega-company backends into Capf functions for seamless Corfu integration.
+(defvar telega-capfs nil)
+(let (capfs)
+  (dolist (backend telega-company-backends)
+    (let* ((backend-str (symbol-name backend))
+           (suffix (string-remove-prefix "telega-company-" backend-str))
+           (capf-name (intern (format "telega-capf-%s" suffix))))
+      (defalias capf-name
+        (cape-capf-interactive
+         (cape-capf-properties
+          (cape-company-to-capf backend)
+          :company-kind (lambda (_) backend))))
+      (push capf-name capfs)))
+  (setq telega-capfs (nreverse capfs)))
+
+;;; Configure nerd-icons-corfu for telega completions
+(after-load! nerd-icons-corfu
+  (alist-setq! nerd-icons-corfu-mapping
+    telega-company-username '(:style "fa" :icon "user" :face default)
+    telega-company-emoji '(:style "md" :icon "sticker_emoji" :face default)
+    telega-company-telegram-emoji '(:style "md" :icon "sticker_emoji" :face default)
+    telega-company-botcmd '(:style "cod" :icon "symbol_class" :face font-lock-builtin-face)
+    telega-company-hashtag '(:style "fa" :icon "hashtag" :face default)
+    telega-company-markdown-precode '(:style "cod" :icon "code_tags" :face font-lock-comment-face)
+    telega-company-quick-reply '(:style "fa" :icon "comment" :face default)))
+
+(defun telega-capf-setup ()
+  "Setup Telega completion-at-point functions for current buffer."
+  (interactive)
+  (when (require 'company nil t)
+    (setq-local completion-at-point-functions
+                (append telega-capfs (list t)))))
+
+(add-hook 'telega-chat-mode-hook #'telega-capf-setup)
 
 (provide 'telega-config)
 ;;; telega-config.el ends here
