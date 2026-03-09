b9f8b894bb9bcc3197778a2696794ec73da0a284
Zhengyi Fu <i@fuzy.me>
2026-03-09 12:25:14 +0800

    Update telega configuration
    * user-lisp/telega-config.el: Remove configuration for
    telega-company.
    (telega-chat-input-complete-function): Set to
    `completion-at-point'.

--- user-lisp/telega-config.el
+++ user-lisp/telega-config.el
@@ -20,13 +20,13 @@
 ;;; Code:
 
 (require 'dotemacs-core)
-(require 'telega-customize)
+
+(setopt telega-translate-to-language-by-default "zh")
+(setopt telega-emoji-use-images nil)
 
 (unless emacs-is-installed-by-nix
   (setq telega-server-command (expand-file-name "telega-server"
                                                 (file-name-directory (locate-library "telega")))))
-(setq telega-translate-to-language-by-default "zh")
-(setq telega-emoji-use-images nil)
 
 ;; (defun telega-config-workaround-gaps-on-graphic-display ()
 ;;   "Enable avatar gap workaround when running on a graphic display."
@@ -51,77 +51,7 @@
 
 (add-hook 'telega-root-mode-hook #'hl-line-mode)
 
-;; Telega company
-
-;; Transform telega-company backends into Capf functions for seamless Corfu integration.
-(defvar telega-capfs nil)
-(let (capfs)
-  (dolist (backend telega-company-backends)
-    (let* ((backend-str (symbol-name backend))
-           (suffix (string-remove-prefix "telega-company-" backend-str))
-           (capf-name (intern (format "telega-capf-%s" suffix))))
-      (defalias capf-name
-        (cape-capf-properties
-         (cape-company-to-capf backend)
-         :company-kind (lambda (_) backend)))
-      (push capf-name capfs)))
-  (setq telega-capfs (nreverse capfs)))
-
-;;; Configure nerd-icons-corfu for telega completions
-(after-load! nerd-icons-corfu
-  (alist-setq! nerd-icons-corfu-mapping
-    telega-company-username '(:style "fa" :icon "user" :face default)
-    telega-company-emoji '(:style "md" :icon "sticker_emoji" :face default)
-    telega-company-telegram-emoji '(:style "md" :icon "sticker_emoji" :face default)
-    telega-company-botcmd '(:style "cod" :icon "symbol_class" :face font-lock-builtin-face)
-    telega-company-hashtag '(:style "fa" :icon "hashtag" :face default)
-    telega-company-markdown-precode '(:style "cod" :icon "code_tags" :face font-lock-comment-face)
-    telega-company-quick-reply '(:style "fa" :icon "comment" :face default)))
-
-(defmacro telega-capf-trigger (capf trigger)
-  `(define-advice ,capf (:around (fn &rest _) trigger)
-     (cape-wrap-trigger fn ,trigger)))
-
-(telega-capf-trigger telega-capf-emoji ?:)
-(telega-capf-trigger telega-capf-telegram-emoji ?:)
-(telega-capf-trigger telega-capf-botcmd ?/)
-(telega-capf-trigger telega-capf-hashtag ?#)
-
-(define-advice telega-capf-username (:around (capf) prefix)
-  (when-let* ((pos (or (save-excursion (re-search-backward "@@" (pos-bol) 'noerror))
-                       (save-excursion (re-search-backward "@" (pos-bol) 'noerror))))
-              (len (length (match-string-no-properties 0)))
-              ((save-excursion (not (re-search-backward "\\s-" pos 'noerror)))))
-    (pcase
-        (funcall capf)
-      (`(,beg ,end ,table . ,plist)
-       (when (<= pos beg (+ len pos))
-         `( ,(+ len pos) ,end ,table
-            :company-prefix-length t
-            :exit-function
-            ,(let ((pos (copy-marker pos))
-                   (end (copy-marker (+ len pos))))
-               (lambda (str status)
-                 (delete-region pos end)
-                 (when-let* ((exit (plist-get plist :exit-function)))
-                   (funcall exit str status))))
-            . ,plist))))))
-
-(advice-add #'telega-capf-username :around #'cape-wrap-buster)
-(advice-add #'telega-capf-emoji :around #'cape-wrap-buster)
-(advice-add #'telega-capf-telegram-emoji :around #'cape-wrap-buster)
-
-(defun telega-capf-setup ()
-  "Setup Telega completion-at-point functions for current buffer."
-  (interactive)
-  (when (require 'company nil t)
-    (setq-local completion-at-point-functions (append telega-capfs (list 'cape-file 'cape-tex)))
-    (setq-local corfu-auto t
-                corfu-quit-no-match nil
-                corfu-auto-trigger "#:/@")))
-
-(define-advice telega-chatbuf-complete (:override () corfu)
-  (completion-at-point))
+(setopt telega-chat-input-complete-function #'completion-at-point)
 
 ;; (add-hook 'telega-chat-mode-hook #'telega-capf-setup)
 (defun telega-completion-setup-corfu ()
