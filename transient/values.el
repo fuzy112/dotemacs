55ea9654c1d9d16c0382a90b8e0122563f0a7346
Zhengyi Fu <i@fuzy.me>
2026-03-18 21:52:00 +0800

    * transient/values.el (ctags-menu): Remove.

--- transient/values.el
+++ transient/values.el
@@ -1,8 +1,3 @@
-((ctags-menu
-  ("--exclude=" ".git")
-  "--recurse"
-  ("--languages=" "C" "C++" "CMake")
-  "--extras=*" "--fields=*" "--kinds-all=*" "--roles-all=*")
- (gptel-menu)
+((gptel-menu)
  (magit-fetch "--prune" "--tags")
  (magit-pull "--autostash"))
