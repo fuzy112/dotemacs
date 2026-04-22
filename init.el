2ac56a8e5da4a24f2b427769d8585567e0445c1d
Zhengyi Fu <i@fuzy.me>
2026-04-22 14:40:21 +0800

    ; Add comments

--- init.el
+++ init.el
@@ -2844,16 +2844,28 @@ not used, but is required by the hook."
           (tags . " %i ")
           (search . " %i ")))
 
+;; Configure org TODO state workflow:
+;; Sequence of states with keybindings and state change hooks:
+;; - TODO: Incomplete active task, accessible via 't' key
+;; - WAIT: Blocked/pending task, accessible via 'w' key; automatically records timestamp and note when entering this state
+;; - |: Separator marks transition from open to closed states
+;; - DONE: Completed task, accessible via 'd' key
+;; - CANCEL: Abandoned task, accessible via 'c' key; automatically records note when entering this state
 (setopt org-todo-keywords '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d)" "CANCEL(c@)")))
 
+;; Set persistent agenda file registry: store list of agenda-enabled files in a dedicated
+;; plaintext file in the org root directory, for easy manual editing and persistence across sessions
 (setopt org-agenda-files (expand-file-name ".agenda-files.txt" org-directory))
 
 (defun +org-has-todo-p ()
+  "Return non-nil if the current buffer contains any active TODO headlines."
   (org-element-map (org-element-parse-buffer 'headline) 'headline
     (lambda (h) (eq (org-element-property :todo-type h) 'todo))
     nil 'first-match))
 
+;; TODO: handle file rename and deletion.
 (defun +org-update-agenda ()
+  "Automatically update the org agenda file list for the current buffer. "
   (when (buffer-file-name)
     (if (and (file-in-directory-p (buffer-file-name) org-directory)
              (+org-has-todo-p))
