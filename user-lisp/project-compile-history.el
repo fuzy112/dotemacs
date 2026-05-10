406d696ef1fe9c9cbba2ae9712b3eb41a9517f17
Zhengyi Fu <i@fuzy.me>
2026-05-10 15:17:00 +0800

    project-compile-history: Add configurable history length, improve save formatting
    * user-lisp/project-compile-history.el
    (project-compile-history-length): New user option.
    (project-compile-history-variable): Add docstring, set
    `history-length' property on newly created per-project history
    variables when `project-compile-history-length' is non-nil.
    (project-compile-history-save): Allow forcing save with universal
    prefix argument, switch to pretty-printing history entries with `pp`
    for improved readability, replace `with-temp-file` with
    `with-work-buffer` and `write-region`.
    (project-compile-history-load): Set `history-length' property on
    loaded per-project history variables when
    `project-compile-history-length' is non-nil.

--- user-lisp/project-compile-history.el
+++ user-lisp/project-compile-history.el
@@ -36,16 +36,39 @@
   "Automatically reload the `project-compile-history-file'."
   :type 'boolean)
 
+(defcustom pch:length nil
+  "Maximum length of history lists of each project.
+When nil, use `history-length', which see."
+  :type '(choice (const :tag "Default value" nil)
+		 natnum)
+  :set (lambda (symbol value)
+	 (set symbol value)
+	 (when (boundp 'pch:obarray)
+	   (obarray-map (lambda (var)
+			  (put var 'history-length value))
+			pch:obarray))
+	 value))
+
 (defvar pch:obarray (obarray-make 16)
   "Obarray for project compile history variables.")
 
 (defvar pch:modified nil)
 
 (defun pch:variable (project)
+  "Return the per-project history variable symbol for PROJECT.
+The variable name is derived from the abbreviated file name of PROJECT's
+root directory, interned in the obarray `project-compile-history-obarray'.
+
+If the returned variable is unbound when this function is called,
+initialize it to nil.  When `project-compile-history-length' is non-nil,
+also set the variable's `history-length' property to the value of
+`project-compile-history-length' during initialization."
   (let* ((root (abbreviate-file-name (project-root project)))
 	 (hist-var (intern root pch:obarray)))
     (unless (boundp hist-var)
-      (set hist-var nil))
+      (set hist-var nil)
+      (when pch:length
+	(put hist-var 'history-length pch:length)))
     hist-var))
 
 (defun pch:read-command (command)
@@ -62,20 +85,21 @@
 
 (defun pch:save ()
   (interactive)
-  (when pch:modified
+  (when (or pch:modified current-prefix-arg)
     (let ((pch:-saving t))
-      (with-temp-file pch:file
-        (insert ";; project-compile-history-file -*- coding: utf-8-emacs; -*-\n\n")
-        (insert "(\n")
-        (obarray-map
-         (lambda (var)
-	   (insert "(")
-	   (prin1 (symbol-name var) (current-buffer))
-	   (insert " . ")
-	   (prin1 (symbol-value var) (current-buffer))
-	   (insert ")\n"))
-         pch:obarray)
-        (insert ")\n"))
+      (with-work-buffer
+	(insert ";; project-compile-history-file -*- coding: utf-8-emacs; -*-\n\n")
+	(insert "(\n")
+	(let ((pp-max-width 200)
+	      (cell (cons nil nil)))
+	  (obarray-map
+	   (lambda (var)
+	     (setcar cell (symbol-name var))
+	     (setcdr cell (symbol-value var))
+	     (pp cell (current-buffer)))
+	   pch:obarray))
+	(insert ")\n")
+	(write-region nil nil pch:file))
       (setq pch:modified nil))))
 
 (defvar pch:-save-timer nil)
@@ -96,7 +120,10 @@
     (goto-char (point-min))
     (let* ((alist (read (current-buffer))))
       (pcase-dolist (`(,name . ,hist) alist)
-	(set (intern name pch:obarray) hist))))
+	(let ((hist-var (intern name pch:obarray)))
+	  (set hist-var hist)
+	  (when pch:length
+	    (put hist-var 'history-length pch:length))))))
   (setq pch:modified nil))
 
 (declare-function compilation-read-command "compile")
