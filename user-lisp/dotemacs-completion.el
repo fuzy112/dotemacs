c6ba1c9ead53d2428a94c1bd37e75c08f77822f9
Zhengyi Fu <i@fuzy.me>
2026-06-08 20:18:02 +0800

    Add Embark target and actions for kmacros
    * user-lisp/dotemacs-completion.el (embark-kmacro-target): New function
    to target textual kmacros in braces at point, inspired by Hyperbole.
    (embark-kmacro-run, embark-kmacro-save, embark-kmacro-name,
    embark-kmacro-bind): New action functions.
    (embark-kmacro-map): New keymap for kmacro actions.
    (embark-target-finders): Add `embark-kmacro-target'.
    (embark-keymap-alist): Add `kmacro' entry.

--- user-lisp/dotemacs-completion.el
+++ user-lisp/dotemacs-completion.el
@@ -400,6 +400,34 @@ targets."
 (advice-add #'embark-completing-read-prompter
             :around #'embark-hide-which-key-indicator)
 
+(defun embark-kmacro-target ()
+  "Target a textual kmacro in braces."
+  (save-excursion
+    (let ((beg (progn (skip-chars-backward "^{}\n") (point)))
+          (end (progn (skip-chars-forward "^{}\n") (point))))
+      (when (and (eq (char-before beg) ?{) (eq (char-after end) ?}))
+        `(kmacro ,(buffer-substring-no-properties beg end)
+                 . (,(1- beg) . ,(1+ end)))))))
+
+(defun embark-kmacro-run (arg kmacro)
+  (interactive "p\nsKmacro: ")
+  (kmacro-call-macro arg t nil (kbd kmacro)))
+
+(defun embark-kmacro-save (kmacro)
+  (interactive "sKmacro: ")
+  (kmacro-push-ring)
+  (setq last-kbd-macro (kbd kmacro)))
+
+(defun embark-kmacro-name (kmacro name)
+  (interactive "sKmacro: \nSName: ")
+  (let ((last-kbd-macro (kbd kmacro)))
+    (kmacro-name-last-macro name)))
+
+(defun embark-kmacro-bind (kmacro)
+  (interactive "sKmacro: \n")
+  (let ((last-kbd-macro (kbd kmacro)))
+    (kmacro-bind-to-key nil)))
+
 (after-load! embark
   (setopt embark-help-key "C-h")
   (setopt embark-cycle-key "C-.")
@@ -407,6 +435,19 @@ targets."
   (setopt embark-indicators '(embark-which-key-indicator
                               embark-highlight-indicator
                               embark-isearch-highlight-indicator))
+  (add-to-list 'embark-target-finders 'embark-kmacro-target)
+
+  (defvar-keymap embark-kmacro-map
+    :doc "Actions on kmacros."
+    :parent embark-general-map
+    "RET" #'embark-kmacro-run
+    "s" #'embark-kmacro-save
+    "n" #'embark-kmacro-name
+    "b" #'embark-kmacro-bind)
+
+  (alist-setq! embark-keymap-alist
+    kmacro '(embark-kmacro-map))
+
   (alist-setq! embark-exporters-alist
     consult-location #'embark-consult-export-location-grep)
   (keymap-set embark-general-map "/" #'embark-history-remove)
