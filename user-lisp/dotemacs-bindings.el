764cdc275a79c5400d22e650e5129a2cfa78993c
Zhengyi Fu <i@fuzy.me>
2026-06-12 10:50:05 +0800

    Add keybinding for `org-insert-link-global`
    * user-lisp/dotemacs-bindings.el (mode-specific-map): Bind "C-l" to
    `org-insert-link-global`.

--- user-lisp/dotemacs-bindings.el
+++ user-lisp/dotemacs-bindings.el
@@ -149,7 +149,8 @@
 
   "C-/" #'apheleia-format-buffer
   "C-k" #'compile
-  "C-p" #'send-password-to-process)
+  "C-p" #'send-password-to-process
+  "C-l" #'org-insert-link-global)
 
 (define-keymap :keymap ctl-x-4-map
   "t" #'eat-other-window
