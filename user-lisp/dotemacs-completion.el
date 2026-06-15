4930e1be82ad0a743e8cf30fb6b8f7083990e753
Zhengyi Fu <i@fuzy.me>
2026-06-15 12:57:55 +0800

    Replace embark-which-key-indicator with embark-mixed-indicator
    * user-lisp/dotemacs-completion.el (embark-indicators): Use
    embark-mixed-indicator.
    (embark-mixed-indicator-delay): Set to 2.
    (embark-which-key-indicator, embark-hide-which-key-indicator):
    Remove.

--- user-lisp/dotemacs-completion.el
+++ user-lisp/dotemacs-completion.el
@@ -374,42 +374,6 @@ If there is no active minibuffer, signal an error."
 
 (setq! prefix-help-command #'embark-prefix-help-command)
 
-(defun embark-which-key-indicator ()
-  "An embark indicator that displays keymaps using which-key.
-The which-key help message will show the type and value of the
-current target followed by an ellipsis if there are further
-targets."
-  (lambda (&optional keymap targets prefix)
-    (require 'which-key)
-    (if (null keymap)
-        (which-key--hide-popup-ignore-command)
-      (which-key--show-keymap
-       (if (eq (plist-get (car targets) :type) 'embark-become)
-           "Become"
-         (format "Act on %s '%s'%s"
-                 (plist-get (car targets) :type)
-                 (embark--truncate-target (plist-get (car targets) :target))
-                 (if (cdr targets) "…" "")))
-       (if prefix
-           (pcase (lookup-key keymap prefix 'accept-default)
-             ((and (pred keymapp) km) km)
-             (_ (key-binding prefix 'accept-default)))
-         keymap)
-       nil nil t (lambda (binding)
-                   (not (string-suffix-p "-argument" (cdr binding))))))))
-
-(defun embark-hide-which-key-indicator (fn &rest args)
-  "Hide the which-key indicator immediately."
-  (require 'which-key)
-  (which-key--hide-popup-ignore-command)
-  (defvar embark-indicators)
-  (let ((embark-indicators
-         (remq #'embark-which-key-indicator embark-indicators)))
-    (apply fn args)))
-
-(advice-add #'embark-completing-read-prompter
-            :around #'embark-hide-which-key-indicator)
-
 (defun embark-kmacro-target ()
   "Target a textual kmacro in braces."
   (save-excursion
@@ -442,7 +406,8 @@ targets."
   (setopt embark-help-key "C-h")
   (setopt embark-cycle-key "C-.")
   (setopt embark-prompter #'embark-keymap-prompter)
-  (setopt embark-indicators '(embark-which-key-indicator
+  (setopt embark-mixed-indicator-delay 2)
+  (setopt embark-indicators '(embark-mixed-indicator
                               embark-highlight-indicator
                               embark-isearch-highlight-indicator))
   (add-to-list 'embark-target-finders 'embark-kmacro-target)
