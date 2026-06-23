e1ec21dd9432ae12db52ba0f54d2d5b28f770f4d
Zhengyi Fu <i@fuzy.me>
2026-06-23 13:53:34 +0800

    Follow system theme via toolkit-theme-set-functions
    * user-lisp/dotemacs-theme.el (dotemacs-theme-follow-toolkit-theme):
    New function that enables Modus themes according to the toolkit
    theme (light or dark).  Add it to `toolkit-theme-set-functions' and
    call it after init to follow the system theme.

--- user-lisp/dotemacs-theme.el
+++ user-lisp/dotemacs-theme.el
@@ -187,5 +187,18 @@ already marked as safe and is not a built-in default Emacs theme."
   (mapc (##load-theme %1 t t)
         custom-enabled-themes))
 
+;;;; Following system theme
+
+(defun dotemacs-theme-follow-toolkit-theme (mode)
+  (mapc #'disable-theme custom-enabled-themes)
+  (let ((theme (pcase mode
+                 ('light 'modus-operandi)
+                 ('dark 'modus-vivendi))))
+    (enable-theme theme)))
+
+(add-hook 'toolkit-theme-set-functions #'dotemacs-theme-follow-toolkit-theme)
+(after-init!
+  (dotemacs-theme-follow-toolkit-theme toolkit-theme))
+
 (provide 'dotemacs-theme)
 ;;; dotemacs-theme.el ends here
