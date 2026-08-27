1d47dda4d38c2afb5779cd50494d034c11d34d78
Zhengyi Fu <i@fuzy.me>
2026-08-27 20:17:00 +0800

    Remove git status from gptel commit context
    `git status' reflects the current working tree and is inaccurate when
    amending, so drop the <git-status> section from the context sent to
    gptel.
    
    * user-lisp/magit-gptel.el (magit-gptel--context): Stop inserting a
    git status section into the generated context.

--- user-lisp/magit-gptel.el
+++ user-lisp/magit-gptel.el
@@ -82,9 +82,6 @@ If you find anything is wrong or unclear, stop immediately without outputing any
     (with-current-buffer (get-buffer-create " *magit-gptel*")
       (setq-local default-directory dir)
       (erase-buffer)
-      (insert "<git-status>")
-      (magit-gptel--run-git "status")
-      (insert "</git-status>\n")
       (insert "<git-diff-staged>")
       (let ((commit (if (member "--amend" args)
 			"HEAD~"
